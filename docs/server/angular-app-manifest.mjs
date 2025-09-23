9bb6863f3bf5112f16d1655c805cea9b35c17c07
Fatih <fatihoezdemir@outlook.com>
2025-09-23 09:03:59 +0200

    Flatten Angular browser build into /docs for GitHub Pages

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -2,7 +2,7 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/iqra2/',
+  baseHref: '/iqra2',
   locale: undefined,
   routes: undefined,
   entryPointToBrowserMapping: {
@@ -14,8 +14,8 @@ export default {
   ]
 },
   assets: {
-    'index.csr.html': {size: 24588, hash: '724d66839cfb27e80820115106eebee6f1bb5190ec7b3b028f3cbcd3626f5206', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17251, hash: 'e1bbdc276177b0e182e7379a80fa7be99b648ea2c7fe1a3f56ac2cce4ded3cb0', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 24587, hash: '4bf342b73090c5a6556226319c173fb3e80cc564ad80e3289adc9a1664fb7b6e', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17250, hash: '0567c0c59a53ea1cb615f4a28e1495678831506befd30efb2392a6569861d433', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-BUFLP657.css': {size: 80634, hash: 'q19qhoq6KRQ', text: () => import('./assets-chunks/styles-BUFLP657_css.mjs').then(m => m.default)}
   },
 };
